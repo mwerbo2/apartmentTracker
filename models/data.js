ae519478c4b5f1816c551bfcdf0029bb89f64a4e
Michael Werbowsky <mwerbo2@gmail.com>
2016-08-10 19:52:23 -0400

    return users from db

--- models/data.js
+++ models/data.js
@@ -1,6 +1,16 @@
 let serialport = require('serialport');
 let portName = '/dev/cu.usbmodem1411';
 let data = {};
+const pg = require('pg-promise')({});
+const config = {
+    host:       process.env.DB_HOST,
+    port:       process.env.DB_PORT,
+    database:   apartmenttracker,
+    user:       process.env.DB_USER,
+    password:   process.env.DB_PASS,
+};
+const _db = pg(config)
+//conect to db pg promise
 
 var sp = new serialport.SerialPort(portName, {
     baudRate: 9600,
@@ -18,6 +28,9 @@ sp.on('data', function(input) {
     console.log(input);
     if (data.id === input){
       console.log("ID= ", input, "Password Accepted!")
+
+      //in this block db.none find their ID and update a column to "active or some shit"
+
     }
     // console.log("this is the id: ", input);
 });
